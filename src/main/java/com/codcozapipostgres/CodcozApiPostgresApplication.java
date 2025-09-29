edc7f395a60cae79d3628d770f8561d2e17d7bf4
Maria Eduarda Araujo <maria.eduarda.araujo1006@gmail.com>
2025-09-29 08:31:22 -0300

    initial commit

---
+++ src/main/java/com/codcozapipostgres/CodcozApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.codcozapipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodcozApiPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodcozApiPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codcozapipostgres/CodcozApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codcozapipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodcozApiPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
